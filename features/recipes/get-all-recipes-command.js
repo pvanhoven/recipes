13b199747a14661573a9f0a842950fd92ee52b26
Phill <phillip.vanhoven@gmail.com>
2021-01-01 14:41:50 -0700

    Removed console.logs

--- features/recipes/get-all-recipes-command.js
+++ features/recipes/get-all-recipes-command.js
@@ -4,7 +4,6 @@ const getAllRecipesCommand = async function getAllRecipesCommand() {
   const config = { region: 'local', endpoint: 'http://localhost:8000' };
   const dynamoDb = new DynamoDBClient(config);
   try {
-    console.log('ret');
     const queryCommand = new QueryCommand({
       TableName: 'Recipes',
       IndexName: 'Seach_by_user',
@@ -19,15 +18,6 @@ const getAllRecipesCommand = async function getAllRecipesCommand() {
     });
 
     const temp = await dynamoDb.send(queryCommand);
-    console.log('done!');
-    if (temp.$metadata.httpStatusCode === 200) {
-      console.log('Cool!');
-    }
-
-    console.log(temp);
-    console.log(temp.Items);
-    console.log(temp.Items.map((i) => `${i.id} ${i.userId}`).join(', '));
-    console.log("ret'd");
     return temp.Items.map((i) => ({
       id: i.id.S,
       userId: i.userId.S,
@@ -35,7 +25,6 @@ const getAllRecipesCommand = async function getAllRecipesCommand() {
     }));
   } catch (err) {
     console.error(err);
-    console.error(JSON.stringify(err, null, 2));
     return [];
   }
 };
